6da3ba87703084d6249d1341aed0eada2ec02656
jerryzj <jerry0715no14@gmail.com>
2018-10-07 23:25:14 +0800

    Add part2

---
+++ hw2/part2/median.cpp
@@ -0,0 +1,26 @@
+#include "median.hpp"
+
+void Median::do_median(){
+    while(1){
+        if(i_red.num_available() > 0 && i_green.num_available() > 0 && i_blue.num_available() > 0){
+            red_ptr = i_red.read();
+            green_ptr = i_green.read();
+            blue_ptr = i_blue.read();
+            for(int i = 0; i < MASK_SIZE; i++){
+                red[i] = red_ptr[i];
+                blue[i] = blue_ptr[i];
+                green[i] = green_ptr[i];
+            }
+            int k = MASK_SIZE / 2;
+            sort(red,red+MASK_SIZE);
+            sort(green,green+MASK_SIZE);
+            sort(blue,blue+MASK_SIZE);
+            o_red.write(red[k]);
+            o_green.write(green[k]);
+            o_blue.write(blue[k]);
+        }
+        else{
+            wait(i_blue.data_written_event());
+        }
+    }
+}
---
+++ hw2/part2/median.hpp
@@ -0,0 +1,122 @@
+#ifndef MEDIAN_HPP
+#define MEDIAN_HPP
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <sys/time.h>
+using namespace std;
+
+#include <systemc>
+using namespace sc_dt;
+using namespace sc_core;
+using sint = sc_int<32>;
+
+
+const int MASK_X = 3;
+const int MASK_Y = 3;
+const int MASK_SIZE = MASK_X * MASK_Y;
+
+SC_MODULE(Testbench){
+public:
+    // Read bmp outputs
+    sc_fifo_out<int*> o_red;
+    sc_fifo_out<int*> o_green;
+    sc_fifo_out<int*> o_blue;
+    // Write bmp inputs
+    sc_fifo_in<int> i_red;
+    sc_fifo_in<int> i_green;
+    sc_fifo_in<int> i_blue;
+
+    SC_CTOR(Testbench) {
+        filter_r[MASK_SIZE] = {0};
+        filter_g[MASK_SIZE] = {0};
+        filter_b[MASK_SIZE] = {0};
+        x = 0;
+        y = 0;
+        temp_r = 0;
+        temp_g = 0;
+        temp_b = 0;
+        width = 0;
+        height = 0;
+        rgb_raw_data_offset = 0;
+        bit_per_pixel = 0;
+        byte_per_pixel = 0;
+        image_s = NULL;
+        image_t = NULL;
+        counter = 0;
+        SC_THREAD(write_bmp);
+        SC_THREAD(read_bmp);
+    }
+private:
+    // read bmp
+    int filter_r[MASK_SIZE];
+    int filter_g[MASK_SIZE];
+    int filter_b[MASK_SIZE];
+    // write bmp
+    unsigned int x, y;
+    int temp_r,temp_g,temp_b;
+    // shared variables
+    unsigned int counter;
+    unsigned int   width;
+    unsigned int   height;
+    unsigned int   rgb_raw_data_offset;
+    unsigned char  bit_per_pixel;
+    unsigned short byte_per_pixel;
+    unsigned char  *image_s;     // source image array
+    unsigned char  *image_t;     // target image array
+    unsigned char header[54] = {
+        0x42,        // identity : B
+        0x4d,        // identity : M
+        0, 0, 0, 0,  // file size
+        0, 0,        // reserved1
+        0, 0,        // reserved2
+        54, 0, 0, 0, // RGB data offset
+        40, 0, 0, 0, // struct BITMAPINFOHEADER size
+        0, 0, 0, 0,  // bmp width
+        0, 0, 0, 0,  // bmp height
+        1, 0,        // planes
+        24, 0,       // bit per pixel
+        0, 0, 0, 0,  // compression
+        0, 0, 0, 0,  // data size
+        0, 0, 0, 0,  // h resolution
+        0, 0, 0, 0,  // v resolution
+        0, 0, 0, 0,  // used colors
+        0, 0, 0, 0   // important colors
+    };
+    void read_bmp();
+    void write_bmp();
+};
+
+SC_MODULE(Median){
+public:
+    // Inputs
+    sc_fifo_in<int*> i_red;
+    sc_fifo_in<int*> i_green;
+    sc_fifo_in<int*> i_blue;
+    // Outputs
+    sc_fifo_out<int> o_red;
+    sc_fifo_out<int> o_green;
+    sc_fifo_out<int> o_blue;
+
+    SC_CTOR(Median) {
+        red_ptr = NULL;
+        green_ptr = NULL;
+        blue_ptr = NULL;
+        red[MASK_SIZE] = {0};
+        green[MASK_SIZE] = {0};
+        blue[MASK_SIZE] = {0};
+        SC_THREAD(do_median);
+    }
+private:
+    int* red_ptr;
+    int* green_ptr;
+    int* blue_ptr;
+    int red[MASK_SIZE];
+    int green[MASK_SIZE];
+    int blue[MASK_SIZE];
+    void do_median();
+};
+#endif
