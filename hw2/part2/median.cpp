e81aa0a220b7e10e7e6846cf7dd213438266eec2
jerryzj <jerry0715no14@gmail.com>
2018-10-08 12:40:27 +0800

    Debugging

--- hw2/part2/median.cpp
+++ hw2/part2/median.cpp
@@ -22,21 +22,26 @@ void Median::do_median(){
         else{
             wait(i_blue.data_written_event());
         } */
-        while(i_update_index.num_available() > 0){
-            int index = i_update_index.read();
-            red_ptr = i_red.read();
-            red[index] = red_ptr;
-            green_ptr = i_green.read();
-            green[index] = green_ptr;
-            blue_ptr = i_blue.read();
-            blue[index] = blue_ptr;
+        if(i_red.num_available() > 0 && i_green.num_available() > 0 && i_blue.num_available() > 0){        
+            while(i_update_index.num_available() > 0){
+                int index = i_update_index.read();
+                red_ptr = i_red.read();
+                red[index] = red_ptr;
+                green_ptr = i_green.read();
+                green[index] = green_ptr;
+                blue_ptr = i_blue.read();
+                blue[index] = blue_ptr;
+            }
+            int k = MASK_SIZE / 2;
+            sort(red,red+MASK_SIZE);
+            sort(green,green+MASK_SIZE);
+            sort(blue,blue+MASK_SIZE);
+            o_red.write(red[k]);
+            o_green.write(green[k]);
+            o_blue.write(blue[k]);
         }
-        int k = MASK_SIZE / 2;
-        sort(red,red+MASK_SIZE);
-        sort(green,green+MASK_SIZE);
-        sort(blue,blue+MASK_SIZE);
-        o_red.write(red[k]);
-        o_green.write(green[k]);
-        o_blue.write(blue[k]);
+        else{
+            wait(i_blue.data_written_event());
+        }
     }
 }
