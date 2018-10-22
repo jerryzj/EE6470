dbb26ec057625e582a684ae51271e2b1b5181a22
jerryzj <jerry0715no14@gmail.com>
2018-10-22 15:19:49 +0800

    [WIP] hw4

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,10 +1,17 @@
 #include "median.hpp"
-
+#include "memory_map.h"
+#include "SimpleBus.h"
 int sc_main(int argc, char **argv){
     Testbench test("test");
     Median median("median");
+    SimpleBus<1,1> bus("bus");
 
-    test.initiator.i_skt(median.t_skt);
+    // Set TLM bus
+    bus.set_clock_period(sc_time(CLOCK_PERIOD, SC_NS));
+    bus.setDecode(0,SOBEL_MM_BASE,SOBEL_MM_BASE+SOBEL_MM_SIZE-1);
+    bus.i_skt[0](median.t_skt);
+    test.initiator.i_skt(bus.t_skt[0]);
+    // do median filter
     test.read_bmp();
     median.set_h_w(test.get_height(), test.get_width());
     sc_start();
---
+++ hw4/memory_map.h
@@ -0,0 +1,8 @@
+#ifndef MEMORY_MAP_H_
+#define MEMORY_MAP_H_
+
+#define SOBEL_MM_BASE (0x90000000)
+#define SOBEL_MM_SIZE (0x00000008)
+#define SOBEL_MM_MASK (0x00000007)
+
+#endif
