8815b5d2ce286a4e6226d8e94a68ca788eb9086f
jerryzj <jerry0715no14@gmail.com>
2018-11-01 07:10:16 +0800

    [WIP] try to use channel to decompose the image

--- hw5/Part2/src/testbench/Op.cpp
+++ hw5/Part2/src/testbench/Op.cpp
@@ -120,21 +120,20 @@ void pool2d(TVMValue stack_value, int arg_num) {
     std::cout << "\t";
     std::cout << "stride: " << stride << "\n";
   }
-  // Run Pooling Engine on SystemC platform
+  /* ==== Run Pooling Engine on SystemC SCML platform ==== */
   // Set Pooling engine
   PoolConfig pool_config;
   ePoolFunction function(is_maximum);
   pool_config.pool_function         = static_cast<uint>(function);
   pool_config.data_cube_in_width    = data_shape.w;
   pool_config.data_cube_in_height   = data_shape.h;
-  pool_config.data_cube_in_channel  = data_shape.c;
+  pool_config.data_cube_in_channel  = data_shape.c/4; // Decompose image
   pool_config.filter_width          = 2;
   pool_config.filter_stride         = stride;
   pool_config.zero_padding          = 0;
-  pool_config.data_cube_out_width  = ((pool_config.data_cube_in_height + 2 * pool_config.zero_padding)
-                                     - pool_config.filter_width) / pool_config.filter_stride + 1;
-  pool_config.data_cube_out_height  = pool_config.data_cube_out_width;
-  pool_config.data_cube_out_channel = pool_config.data_cube_in_channel;
+  pool_config.data_cube_out_width   = output_shape.w;
+  pool_config.data_cube_out_height  = output_shape.h;
+  pool_config.data_cube_out_channel = output_shape.c/4;
   pool_config.data_in_address       = POOL_BUFFER_ADDRESS;
   pool_config.data_out_address      = POOL_BUFFER_ADDRESS
                                       + (pool_config.data_cube_in_channel
@@ -149,35 +148,55 @@ void pool2d(TVMValue stack_value, int arg_num) {
   uint result_num(pool_config.data_cube_out_channel
                   * pool_config.data_cube_out_height
                   * pool_config.data_cube_out_width);
-  testbench.LoadTestData(GLOBAL_BUFFER_ADDRESS, data_ptr, test_data_num);
-
-  /* Configure DMA to load test data into buffer from RAM */
-  vector<DmaChConfig> dma_config(1);
-  dma_config[0].channel_enable  = 1;
-  dma_config[0].source_address  = GLOBAL_BUFFER_ADDRESS;
-  dma_config[0].dest_address    = pool_config.data_in_address;
-  dma_config[0].transfer_length = test_data_num * sizeof(float);
-  dma_config[0].transfer_type   = 3;
-  dma_config[0].line_length     = 0;
-  dma_config[0].line_stride     = 0;
-  testbench.ConfigPoolDMA(dma_config);
-
-  /* Configure pooling engine to process test data */
-  testbench.ConfigPoolEngine(pool_config);
-
-  /* Configure DMA to move results from buffer into RAM */
-  dma_config[0].channel_enable  = 1;
-  dma_config[0].source_address  = pool_config.data_out_address;
-  dma_config[0].dest_address    = GLOBAL_BUFFER_ADDRESS;
-  dma_config[0].transfer_length = result_num * sizeof(float);
-  dma_config[0].transfer_type   = 3;
-  dma_config[0].line_length     = 0;
-  dma_config[0].line_stride     = 0;
-  testbench.ConfigPoolDMA(dma_config);
-
-  /* Get result from RAM through debug transport (no timing effort) */
-  testbench.GetResult(GLOBAL_BUFFER_ADDRESS, output_ptr, result_num);
-
+  
+  auto* frame_ptr = getPtr(stack_array[1].data,
+                          stack_array[1].byte_offset);
+  for(unsigned int c = 0; c < 4; c++){
+    ShapeTy decomposed = ShapeTy(1, data_shape.c/4, data_shape.h, data_shape.w);
+    const auto Iidx = data_shape.Idx(c * data_shape.c/4, data_shape.h, data_shape.w);
+    const auto Oidx = output_shape.Idx(c * output_shape.c/4, 0, 0);
+    cout<<"====Debug==="<<endl;
+    cout<<"Data input channel = "<<c<<endl;
+    printTensor(decomposed, &data_ptr[Iidx]);
+    // Load Test Data from DRAM to GLOBAL BUFFER
+    testbench.LoadTestData(GLOBAL_BUFFER_ADDRESS, &data_ptr[Iidx], test_data_num);
+    // Configure DMA to load test data into Pooling Engine 
+    vector<DmaChConfig> dma_config(1);
+    dma_config[0].channel_enable  = 1;
+    dma_config[0].source_address  = GLOBAL_BUFFER_ADDRESS;
+    dma_config[0].dest_address    = pool_config.data_in_address;
+    dma_config[0].transfer_length = test_data_num * sizeof(float);
+    dma_config[0].transfer_type   = 3;
+    dma_config[0].line_length     = 0;
+    dma_config[0].line_stride     = 0;
+    testbench.ConfigPoolDMA(dma_config);
+    // Configure DMA to load test data into Pooling Engine 
+    vector<DmaChConfig> dma_config(1);
+    dma_config[0].channel_enable  = 1;
+    dma_config[0].source_address  = GLOBAL_BUFFER_ADDRESS;
+    dma_config[0].dest_address    = pool_config.data_in_address;
+    dma_config[0].transfer_length = test_data_num * sizeof(float);
+    dma_config[0].transfer_type   = 3;
+    dma_config[0].line_length     = 0;
+    dma_config[0].line_stride     = 0;
+    testbench.ConfigPoolDMA(dma_config);
+    // Configure pooling engine to process test data 
+    testbench.ConfigPoolEngine(pool_config);
+    // Configure DMA to move results from buffer into RAM 
+    dma_config[0].channel_enable  = 1;
+    dma_config[0].source_address  = pool_config.data_out_address;
+    dma_config[0].dest_address    = GLOBAL_BUFFER_ADDRESS;
+    dma_config[0].transfer_length = result_num * sizeof(float);
+    dma_config[0].transfer_type   = 3;
+    dma_config[0].line_length     = 0;
+    dma_config[0].line_stride     = 0;
+    testbench.ConfigPoolDMA(dma_config);
+    //Get result from RAM through debug transport (no timing effort)
+    testbench.GetResult(GLOBAL_BUFFER_ADDRESS, &output_ptr[Oidx], result_num);
+    cout<<"====Debug==="<<endl;
+    cout<<"Data input channel = "<<c<<endl;
+    printTensor(decomposed, &data_ptr[Oidx]);
+  }
   if (print_data) {
     // In this example, only prints the first channel
     std::cout << "Input: " << "\n";
