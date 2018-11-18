2d286e060d6082d518a4f5d18498ec05f07c68cf
jerryzj <jerry0715no14@gmail.com>
2018-11-18 19:43:26 +0800

    debug change err limit

--- midterm/MobileNet_layer3_SCML/src/testbench/Config.h
+++ midterm/MobileNet_layer3_SCML/src/testbench/Config.h
@@ -8,6 +8,6 @@ const bool print_data = true;
 // Set the print-out precision for Ops
 const int precision = 5;
 // Set the hard limit of error ratio while comparing results
-const double ratio_limit = 0.0001;
+const double ratio_limit = 0.001;
 // Set the hard limit of error while comparing results
-const double error_limit = 0.0005;
+const double error_limit = 0.005;
