dfe359263a467b9622beef35224d398228f11dac
jerryzj <jerry0715no14@gmail.com>
2018-11-18 16:51:07 +0800

    [Debug] print all channels

--- midterm/MobileNet_layer3_SCML/src/testbench/Op.cpp
+++ midterm/MobileNet_layer3_SCML/src/testbench/Op.cpp
@@ -189,9 +189,9 @@ void pool2d(TVMValue stack_value, int arg_num) {
   if (print_data) {
     // In this example, only prints the first channel
     std::cout << "Input: " << "\n";
-    printTensor(data_shape, data_ptr, 0);
+    printTensor(data_shape, data_ptr);
     std::cout << "Output: " << "\n";
-    printTensor(output_shape, output_ptr, 0);
+    printTensor(output_shape, output_ptr);
   }
 
   // Real compuatations
@@ -266,9 +266,9 @@ void norm2d(TVMValue stack_value, int arg_num) {
   if (print_data) {
     // In this example, only prints the first channel
     std::cout << "Input: " << "\n";
-    printTensor(data_shape, data_ptr, 0);
+    printTensor(data_shape, data_ptr);
     std::cout << "Output: " << "\n";
-    printTensor(output_shape, output_ptr, 0);
+    printTensor(output_shape, output_ptr);
   }
 
   // Real compuatations
@@ -311,9 +311,9 @@ void relu2d(TVMValue stack_value, int arg_num) {
   if (print_data) {
     // In this example, only prints the first channel
     std::cout << "Input: " << "\n";
-    printTensor(data_shape, data_ptr, 0);
+    printTensor(data_shape, data_ptr);
     std::cout << "Output: " << "\n";
-    printTensor(output_shape, output_ptr, 0);
+    printTensor(output_shape, output_ptr);
   }
 
   // Real compuatations
