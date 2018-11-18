0b2a93f7ee360f1a163ea34743f3820f921f2335
jerryzj <jerry0715no14@gmail.com>
2018-11-18 16:25:05 +0800

    [Debug] change outpur precision

--- midterm/MobileNet_layer3_SCML/src/testbench/Config.h
+++ midterm/MobileNet_layer3_SCML/src/testbench/Config.h
@@ -6,7 +6,7 @@ using DataTy = float;
 const bool print_shape = true;
 const bool print_data = true;
 // Set the print-out precision for Ops
-const int precision = 1;
+const int precision = 5;
 // Set the hard limit of error ratio while comparing results
 const double ratio_limit = 0.0001;
 // Set the hard limit of error while comparing results
