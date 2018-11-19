915b797f96c52ba63a7d4a1022eea881d057c6e2
jerryzj <jerry0715no14@gmail.com>
2018-11-19 15:57:09 +0800

    [SCML] fix typo

--- midterm/MobileNet_SCML/src/conv_engine/conv_engine.cpp
+++ midterm/MobileNet_SCML/src/conv_engine/conv_engine.cpp
@@ -89,7 +89,7 @@ void ConvEngine::DoConv() {
 	// Caclulate conv engine delay from HLS 2D convolution simulation
 	int delay = 0;
 	if(filter_stride.get() == 1){
-		if(data_cube_in_channel.get() == 7){// 7*7 delay
+		if(data_cube_in_height.get() == 7){// 7*7 delay
 			delay = data_cube_out_channel * data_cube_in_channel * 4900;
 		}
 		else{ // 17*17 delay
